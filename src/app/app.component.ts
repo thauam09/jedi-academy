8f8380e318873fe04b8e653a4a9dd373533ca457
thauam09 <thauam_09martins@hotmail.com>
2021-11-01 09:30:07 -0300

    modulos, componentes, diretivas e property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  students: Student[] = [
+    { name: 'Luke', isJedi: true, temple: 'Coruscant' },
+    { name: 'Leia', isJedi: false },
+    { name: 'Han Solo', isJedi: false },
+  ];
 }
